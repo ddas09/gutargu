87d260e170848edaa11058171c09b2f4e78365ab
Dipankar <dipankar62918@gmail.com>
2024-12-17 00:30:26 +0530

    Added chat components

--- gutargu-frontend/src/components/detail/Detail.tsx
+++ gutargu-frontend/src/components/detail/Detail.tsx
@@ -4,9 +4,65 @@ const Detail = () => {
 
   return (
     <div className="detail">
-        Detail
+        <div className="user">
+          <img src="./avatar.png" alt="" />
+          <h2>Dip Das</h2>
+          <p>This is my status</p>
+        </div>
+
+        <div className="info">
+          <div className="option">
+            <div className="title">
+              <span>Chat Settings</span>
+              <img src="./arrowUp.png" alt="" />
+            </div>
+          </div>
+          <div className="option">
+            <div className="title">
+              <span>Privacy & Help</span>
+              <img src="./arrowUp.png" alt="" />
+            </div>  
+          </div>
+          <div className="option">
+            <div className="title">
+              <span>Shared Photos</span>
+              <img src="./arrowDown.png" alt="" />
+            </div>
+            <div className="photos">
+              <div className="photoItem">
+                <div className="photoDetail">
+                  <img src="https://cdn.pixabay.com/photo/2023/01/08/14/22/sample-7705346_640.jpg" alt="" />
+                  <span>My photo</span>
+                </div>
+                <img src="./download.png" className='icon' alt="" />
+              </div>
+              <div className="photoItem">
+                <div className="photoDetail">
+                  <img src="https://cdn.pixabay.com/photo/2023/01/08/14/22/sample-7705346_640.jpg" alt="" />
+                  <span>My photo</span>
+                </div>
+                <img src="./download.png" className='icon' alt="" />
+              </div>
+              <div className="photoItem">
+                <div className="photoDetail">
+                  <img src="https://cdn.pixabay.com/photo/2023/01/08/14/22/sample-7705346_640.jpg" alt="" />
+                  <span>My photo</span>
+                </div>
+                <img src="./download.png" className='icon' alt="" />
+              </div>
+            </div>
+          </div>
+          <div className="option">
+            <div className="title">
+              <span>Shared Files</span>
+              <img src="./arrowUp.png" alt="" />
+            </div>  
+          </div>
+          <button>Block User</button>
+          <button className='logout'>Logout</button>
+        </div>
     </div>
-  )
+  );
 };
 
 export default Detail;
